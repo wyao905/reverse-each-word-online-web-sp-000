8af1e53c293b17158b948ada028aa138682b0ac1
Warren Yao <warrenyca905@gmail.com>
2019-05-19 16:57:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
-  array = sentence.split(" ").collect do |word|
+  array = sentence.split(" ")
+  array.collect do |word|
     word.reverse
   end
   return array.join(" ")
